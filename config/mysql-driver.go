22d6fd4adba0aa7a8c1d4fb7c7874fd69367560e
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-08-23 09:01:05 +0700

    DB Setting Default

--- config/mysql-driver.go
+++ config/mysql-driver.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func ConnectToDB() *sql.DB {
+	db, err := sql.Open("mysql", "root:@tcp(192.168.0.112:3306)/accountserviceapp")
+
+	if err != nil {
+		log.Fatal("error sql Open ", err.Error())
+	}
+
+	errPing := db.Ping()
+	if errPing != nil {
+		log.Fatal("error connect to db ", errPing.Error())
+	} else {
+		fmt.Println("success connect to DB")
+	}
+
+	return db
+}
