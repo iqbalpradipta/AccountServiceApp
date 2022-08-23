43422b5a65c4489389e2338a64c5622fb0fcb7d4
iqbalpradipta <iqbalpradipta2@gmail.com>
2022-08-23 08:59:38 +0700

    default

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"projectapp/config"
+)
 
+func main() {
+	db:= config.ConnectToDB()
+	defer db.Close()
+	fmt.Print("MENU UTAMA: ")
+	fmt.Print("\n1.Add Accout\n2.Login\n3.Read Account\n4.Update Account\n5.Delete Account\n6.TopUp Account\n7.Transfer\n8.History TopUp\n9.History Transfer\n10.Lihat Profil user lain\n0.Exit\n")
+	fmt.Println("Masukan pilihan anda: ")
+	var pilihan int
+	fmt.Scanln(&pilihan)
 }
