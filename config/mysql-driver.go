650a52d2f20fc2edf388d8016d23ad392beff795
Irsan wally <irsanw4lly@gmail.com>
2022-08-24 12:51:00 +0900

    default

--- config/mysql-driver.go
+++ config/mysql-driver.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
--- entities/user.go
+++ entities/user.go
@@ -1,13 +1,13 @@
 package entities
 
-type User struct{
-	id				string
-	user_id			string
-	name			string
-	password		string
-	alamat			string
-	jenis_kelamin	string
-	telp			string
-	saldo			int
-	update_at		string
+type User struct {
+	id            string
+	user_id       string
+	name          string
+	password      string
+	alamat        string
+	jenis_kelamin string
+	telp          string
+	saldo         int
+	update_at     string
 }
